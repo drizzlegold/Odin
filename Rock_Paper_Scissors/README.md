b26ea488d07f39eb9424d2f88b89702600dc5ec5
drizzlegold <goldendrizzle66@gmail.com>
2023-04-06 08:28:26 +0100

    Javascript tutorial

---
+++ Rock_Paper_Scissors/README.md
@@ -0,0 +1,3 @@
+Rock, Paper, Scissors.
+
+This, being my first javascript  project  is a simple rock, paper, scissors game where the user playsagainst the computer.
---
+++ Rock_Paper_Scissors/index.js
@@ -0,0 +1,54 @@
+
+function UserChoice(){
+    let userInput=(prompt("Enter your choice: "))
+    
+    let capsInput= userInput.toUpperCase();
+    
+    return(capsInput)
+    
+}
+
+//UserChoice();
+
+function ComputerChoice(){
+    let options=["ROCK", "PAPER", "SCISSORS"];
+    let choice = Math.floor(Math.random()* options.length)
+    return(options[choice])
+}
+
+//ComputerChoice();
+ 
+ function playRound(){
+     let user= UserChoice();
+     let computer= ComputerChoice();
+     
+     //console.log(user, computer)
+    
+    switch(true){
+         case(user != "ROCK" || user || "PAPER" || user != "SCISSORS"):
+         console.log(user + " is not a valid entry")
+         break;
+         
+         case(user === computer):
+         console.log("Tie game. you both chose " + computer)
+         break;
+         
+         case(user ==="ROCK" && computer === "paper"):
+         console.log(" You lose " + computer + " beats" + user)
+         break;
+         
+         case(user === "PAPER" && computer === "SCISSORS"):
+         console.log(" You lose " + computer + " beats" + user)
+         break;
+         
+         case(user === "SCISSORS" && computer === "ROCK"):
+         console.log(" You lose " + computer + " beats " + user)
+         break;
+         
+        default:
+        console.log(" You win " + user + " beats " + computer)
+         
+     }
+ }
+ 
+ playRound();
